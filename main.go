bfa14a9bf0a05e5bbd0faa3e0a527ccdb08f69fd
Chris Meyers <chris@chrismeyers.net>
2022-04-13 21:08:03 -0600

    Cleared screen between board renderings

--- main.go
+++ main.go
@@ -3,15 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 const STARTING_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 func main() {
-	board := NewBoard()
-	board.Render(os.Stdout)
+	clearScreen()
 
-	fmt.Println()
-
+	fmt.Println("Render FEN")
 	RenderFEN(os.Stdout, STARTING_FEN)
+	time.Sleep(1 * time.Second)
+
+	clearScreen()
+
+	board := NewBoard()
+	for i := 0; i < 3; i++ {
+		fmt.Printf("Render Board %d\n", i+1)
+		board.Render(os.Stdout)
+		time.Sleep(1 * time.Second)
+		clearScreen()
+	}
+}
+
+func clearScreen() {
+	fmt.Print("\033[2J")
+	fmt.Print("\033[H")
 }
